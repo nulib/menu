e058d5a9a84a7f4d7103e01686b856780a919d7c
Chris Syversen <csyversen@gmail.com>
2014-06-02 14:54:16 -0500

    changed root to images#index

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Menu::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'images#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
