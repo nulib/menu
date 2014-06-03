ae55c973965025eca07a7d2c717925b7e738c5fe
Paul Clough <p-clough@northwestern.edu>
2014-06-03 14:29:25 -0500

    Get file list from ../davies

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,21 @@
 class Image < ActiveRecord::Base
+
+  def self.get_files
+    dir_location = "davies"
+    Dir.entries( dir_location ).each do |file|
+      if Image.verify_file( file, dir_location )
+        Image.create( filename: file, location: dir_location )
+      end
+    end
+  end
+
+  private
+
+    def self.verify_file( file, dir_location )
+      if Image.find_by( filename: file, location: dir_location ) ||
+        file == '.' ||
+        file == '..' ||
+        File.extname( file ) !=~ /tif|tiff/i
+      end
+    end
 end
