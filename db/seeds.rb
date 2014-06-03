cb2a0a9e6f8b844f4956ee5e3ec14ab3a4dcbe05
Paul Clough <p-clough@northwestern.edu>
2014-06-03 11:49:48 -0500

    Add seed DB data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |x|
+  Image.create!( filename: "image#{x}.tif", location: "gandalf" )
+end
