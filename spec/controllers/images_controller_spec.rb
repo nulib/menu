ea38274d3d318f7f93d43bd6025abcb515e4bbdb
egspoony <edgar-garcia@northwestern.edu>
2014-06-04 17:05:50 -0500

    Added update rspec tests to images_controller.

--- spec/controllers/images_controller_spec.rb
+++ spec/controllers/images_controller_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe ImagesController, :type => :controller do
         post(:create, :image => {filename: 'test.tif', location: 'gandalf'})
       end.to change(Image, :count).by(1)
     end
+  end
 
   describe "DESTROY image" do
 
@@ -19,6 +20,26 @@ RSpec.describe ImagesController, :type => :controller do
       expect do 
         delete(:destroy, :id => @image.to_param)
       end.to change(Image, :count).by(-1)
+    end
+  end
+
+  describe "UPDATE image" do
+
+    it "locates the requested image" do
+      put(:update, id: @image, image: {filename: 'different.tif', location: 'gandalf2'})
+      expect(assigns(:image)).to eq(@image)
+    end
+
+    it "changes the images's attributes" do
+      put(:update, id: @image, image: {filename: 'different.tif', location: 'gandalf2'})
+      @image.reload
+      expect(@image.filename).to eq('different.tif')
+      expect(@image.location).to eq('gandalf2')
+    end
+
+    it "redirects to the updated image" do
+      put(:update, id: @image, image: {filename: 'different.tif', location: 'gandalf2'})
+      expect(response).to redirect_to(@image)
     end
   end
 end
