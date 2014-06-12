4ac75098f61791399d5af26e1e61be4bfd775b7b
Paul Clough <p-clough@northwestern.edu>
2014-06-12 14:08:38 -0500

    Change image file location to /dropbox

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,7 +4,7 @@ class ImagesController < ApplicationController
   # GET /images
   # GET /images.json
   def index
-    GetImages.current_images( 'davies' )
+    GetImages.current_images( '/dropbox' )
     @images = Image.all
   end
 
