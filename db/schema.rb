fc63a3ded7dfa80cb9fc0bae7dc4df9d1cb92a92
Chris Syversen <csyversen@gmail.com>
2014-06-16 16:33:46 -0500

    remove redundant lines in controller, add updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140613000000) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "images", force: true do |t|
     t.string   "filename"
     t.string   "location"
