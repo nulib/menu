aec76073e49f48ad0e4b567db570d3f9e5891c53
Paul Clough <p-clough@northwestern.edu>
2014-06-17 10:39:11 -0500

    Convert the thumbnail to JPEG format

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ActiveRecord::Base
-  has_attached_file :proxy, :styles => { :thumb => "100x100" }
+  has_attached_file :proxy, :styles => { :thumb => [ "100x100", :jpg ] }
 
   validates_attachment_content_type :proxy, :content_type => "image/tiff"
   validates :filename, :uniqueness => true
