e7a2897fd036b5bbbaa866d39ac2e704bb9a4087
Paul Clough <p-clough@northwestern.edu>
2014-06-17 15:01:45 -0500

    Only add files that are in a sub-folder. Use the sub-folder label as the Job ID.

--- app/helpers/get_images.rb
+++ app/helpers/get_images.rb
@@ -1,9 +1,11 @@
 module GetImages
 
   def self.current_images( location )
-    Dir.glob( "#{location}/*" ).each do |file|
+    Dir.glob( "#{location}/**/*" ).each do |file|
       f = File.open( file )
-      Image.create( filename: file, proxy: f )
+      path = file.split( '/' )
+      next unless File.file?( file ) && path.size == 4
+      Image.create( filename: file, job_id: path[ -2 ], proxy: f )
       f.close
     end
   end
