16e16d967d81f8bff51e07af260e596c15553936
Paul Clough <p-clough@northwestern.edu>
2014-06-18 15:14:08 -0500

    Updating tests for Job ID

--- app/helpers/get_images.rb
+++ app/helpers/get_images.rb
@@ -1,10 +1,11 @@
 module GetImages
 
   def self.current_images( location )
+    path_length = location.split( '/' ).count
     Dir.glob( "#{location}/**/*" ).each do |file|
       f = File.open( file )
       path = file.split( '/' )
-      next unless File.file?( file ) && path.size == 4
+      next unless File.file?( file ) && path.size == path_length + 2
       Image.create( filename: file, job_id: path[ -2 ], proxy: f )
       f.close
     end
