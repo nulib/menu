13a184959ef6fc1bda00799a5334477048a9b8a9
Chris Syversen <csyversen@gmail.com>
2014-06-19 17:27:30 -0500

    changed get_images to delete stale records

--- app/helpers/get_images.rb
+++ app/helpers/get_images.rb
@@ -2,13 +2,26 @@ module GetImages
 
   def self.current_images( location )
     path_length = location.split( '/' ).count
+    imgs = []
+
     Dir.glob( "#{location}/**/*" ).each do |file|
       f = File.open( file )
       path = file.split( '/' )
-      next unless File.file?( file ) && path.size == path_length + 2
-      Image.create( filename: file, job_id: path[ -2 ], proxy: f )
+      if File.file?( file ) && path.size == path_length + 2
+        i = Image.find_by( filename: file, job_id: path[ -2 ] ) #, proxy: f
+        if i == nil
+          i = Image.create( filename: file, job_id: path[ -2 ], proxy: f )
+        end
+        imgs << i.id
+      end
       f.close
     end
-  end
 
+    # delete img records in db if they are no longer in the file system
+    stale_records = Image.where.not(id: imgs)
+    stale_records.each do |i|
+      i.destroy
+    end
+
+  end
 end
