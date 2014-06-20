7ffce47464d7f0f02fa2c34d35ce180bc653a4bf
Chris Syversen <csyversen@gmail.com>
2014-06-20 09:39:20 -0500

    in progress checkin of tests

--- spec/helpers/get_images_spec.rb
+++ spec/helpers/get_images_spec.rb
@@ -6,10 +6,10 @@ describe GetImages do
     loc = "#{Rails.root}/spec/dropbox_valid"
     path_length = loc.split( '/' ).count
     contents = Dir.glob( "#{loc}/**/*" )
-    contents.select! do |entry|
-      File.file?( entry ) || entry.split( '/' ).count == path_length + 2
-    end
-    
+    # contents.select! do |entry|
+    #   File.file?( entry ) || entry.split( '/' ).count == path_length + 2
+    # end
+    #
     GetImages.current_images( loc )
     expect( Image.all.count ).to eql( contents.count )
   end
@@ -19,5 +19,24 @@ describe GetImages do
     GetImages.current_images( loc )
     expect( Image.all.count ).to eql( 0 )
   end
+
+  it "removes stale records from the db" do
+    require 'fileutils'
+
+    loc = "spec/stale_images"
+
+    contents = Dir.glob( "#{loc}/123/*" )
+
+    GetImages.current_images( loc )
+    expect( Image.all.count ).to eql( contents.count )
+
+    FileUtils.mv('spec/stale_images/123/technology.tiff', 'spec/stale_images/technology.tiff')
+
+    GetImages.current_images( loc )
+    expect( Image.all.count ).to eql( 0 )
+
+
+  end
+
 
 end
