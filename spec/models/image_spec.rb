3717aa1cb88f15a0eac400d495bd7375a337b47f
Paul Clough <p-clough@northwestern.edu>
2014-06-23 11:44:27 -0500

    Use #create rather than #save as the trigger for add_minimal_xml

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -5,4 +5,12 @@ describe Image do
     @image = Image.create( job_id: 'test' )
     expect( @image.xml ).to be_truthy
   end
+
+  context "a new image" do
+    it "has the default VRA XML in the xml attribute" do
+      @image = Image.create( job_id: 'test' )
+      default_xml = File.read( 'app/assets/xml/vra_minimal.xml' )
+      expect( @image.xml ).to eql( default_xml )
+    end
+  end
 end
