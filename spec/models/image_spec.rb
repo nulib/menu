aafebc0af3e28f11ea7cef269f3b34fcdbe217cb
bmquinn <brendan-quinn@northwestern.edu>
2014-07-03 13:50:50 -0500

    Update Image model and spec for image_xml db column

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 describe Image do
   it "has a non-empty xml attribute" do
     @image = Image.create( job_id: 'test' )
-    expect( @image.xml ).to be_truthy
+    expect( @image.image_xml ).to be_truthy
   end
 
   context "a new image" do
     it "has the default VRA XML in the xml attribute" do
       @image = Image.create( job_id: 'test' )
       default_xml = File.read( 'app/assets/xml/vra_minimal.xml' )
-      expect( @image.xml ).to eql( default_xml )
+      expect( @image.image_xml ).to eql( default_xml )
     end
   end
 end
