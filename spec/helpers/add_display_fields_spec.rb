bed1cb67a1306723e679e5cbe014424c18bd59f6
Paul Clough <p-clough@northwestern.edu>
2014-10-07 00:01:27 -0500

    DIL-3868: Add display elements programatically
    * Add equivalent-xml gem
    * Add display elements to VRA image record
    * Add a <vra:display> element to each *Set element in the VRA record,
      except for dateSet and subjectSet.
    * Add pry-byebug gem for pry debugging funsies
    * Add and populate display elements to VRA:image
    * Ignore database.yml.pg
    * Refactor adding display elements

---
+++ spec/helpers/add_display_fields_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "Adds display fields to VRA:image" do
+  it "successfully" do
+    vra_without_display = File.read( Rails.root.join( 'spec', 'fixtures', 'vra_no_display.xml' ))
+    vra_with_display = File.read( Rails.root.join( 'spec', 'fixtures', 'vra_display_with_content.xml' ))
+
+    vra = TransformXML.add_display_elements( vra_without_display )
+
+    expect( vra ).to be_equivalent_to( vra_with_display ).respecting_element_order
+  end
+end
+
