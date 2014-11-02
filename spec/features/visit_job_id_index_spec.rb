4bd5c6d3f6b4ad44c9f9bb904d0925e361a6853a
Paul Clough <p-clough@northwestern.edu>
2014-11-02 01:16:18 -0500

    Job listing should be a list of links

--- spec/features/visit_job_id_index_spec.rb
+++ spec/features/visit_job_id_index_spec.rb
@@ -7,11 +7,11 @@ describe "visiting the home page" do
     expect( page ).to have_content( "Job IDs" )
   end
 
-  it "displays a list of Job IDs" do
+  it "displays a list of linked Job IDs" do
     job = Job.create( job_id: 123 )
 
     visit root_path
 
-    expect( page ).to have_content( "123" )
+    expect( page ).to have_css( "li.job_link>a" )
   end
 end
