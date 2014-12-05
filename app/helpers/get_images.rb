518151bf394c79f4c6e458d8b2b45389cd9d1631
Paul Clough <p-clough@northwestern.edu>
2014-12-05 15:03:39 -0600

    Add some logging

--- app/helpers/get_images.rb
+++ app/helpers/get_images.rb
@@ -7,8 +7,10 @@ module GetImages
 
     imgs = []
     subdirs = Dir.glob( "#{location}/**/*" )
+    logger.debug( subdirs )
     subdirs.delete_if { |dir| dir =~ /_completed/ }
     subdirs.each do |file|
+      logger.debug( file )
       imgs << find_or_create_image(file)
     end
 
