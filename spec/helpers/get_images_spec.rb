f4183b046755385db04eaf33329bf2e037315851
Chris Syversen <csyversen@gmail.com>
2014-12-08 14:40:27 -0600

    added test for remove_stale_images

--- spec/helpers/get_images_spec.rb
+++ spec/helpers/get_images_spec.rb
@@ -13,8 +13,12 @@ describe GetImages do
     expect( Image.all.count ).to eql( 0 )
   end
 
-  xit "removes stale images" do
-    pending "create images in db that don't exist in file system"
+  # The empty array sent to the remove_stale_images function implies that there are no
+  # image files on the file system, so the Image model that was just created is stale
+  it "removes stale images" do
+    Image.create(filename: "cool_test", job_id: "98765")
+    GetImages.remove_stale_images( [] )
+    expect( Image.all.count ).to eql( 0 )
   end
 
 end
