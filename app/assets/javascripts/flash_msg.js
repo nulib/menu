4b40ebf8abf9ea46b7cc6e1b0736239ff91b003f
Jennifer Lindner <jenlindner@gmail.com>
2015-03-03 11:16:54 -0600

    making flash messages appear if images doens't publish valid vra

--- app/assets/javascripts/flash_msg.js
+++
@@ -1,9 +0,0 @@
-$(document).ajaxError(function(event, request) {
-  var msg = request.getResponseHeader('X-Message');
-    if (msg) {
-      alert(msg);
-      $('#errors').append(msg);
-      $('#errors').show();
-      }
-
-});
