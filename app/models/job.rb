5479b24263d0b6937e1226acc90770fad9b4b081
bmquinn <brendan-quinn@northwestern.edu>
2015-03-30 15:35:55 -0500

    Order images by filename within jobs via scope block

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,5 @@
 class Job < ActiveRecord::Base
-  has_many :images
+  has_many :images,-> { order "filename asc" }
 
   validates :job_id, uniqueness: true
 end
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -13,4 +13,12 @@ describe Job do
 
     expect( job.images.count ).to eql( 1 )
   end
+
+  it 'orders images by filename' do
+    job = Job.create( job_id: 123 )
+    image1 = job.images.create(filename: "002.tif")
+    image2 = job.images.create(filename: "001.tif")
+
+    expect( job.images.to_a ).to eql( [image2, image1] )
+  end
 end
