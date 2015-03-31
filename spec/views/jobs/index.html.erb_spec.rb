745c08c81f64821f57b520dcba0cbedeb007b8c9
Jennifer Lindner <jenlindner@gmail.com>
2015-03-31 10:59:57 -0500

    using factorygirl

--- spec/views/jobs/index.html.erb_spec.rb
+++ spec/views/jobs/index.html.erb_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
+require 'spec_helper'
 
 describe 'jobs/index.html.erb', :type => :view do
   it 'shows how many images are in each job'  do
-    assign(:jobs, [stub_model(Job) {|job| job.job_id = "1111"}])
-    stub_model(Image) {|image| image.job_id = "1111"}
-    stub_model(Image) {|image| image.job_id = "1111"}
+
+    job1 = create(:job_with_images)
+    job2 = create(:job_with_images)
+    @jobs = [job1, job2]
 
     render :template => 'jobs/index.html.erb'
 
-
-    expect(rendered).to have_content "Job IDs 1111 (2)"
+    expect(rendered).to have_content "#{job1.job_id} (#{job1.images.size})"
   end
 end
--- spec/views/jobs/show.html.erb_spec.rb
+++ spec/views/jobs/show.html.erb_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 describe 'jobs/show.html.erb', :type => :view do
 
   it 'contains links that say Delete instead of Destroy' do
-    assign(:images, [stub_model(Image) {|image| image.job_id = "test_id"}])
+    image1 = create(:image)
+    image2 = create(:image)
+    @images = [image1, image2]
+
     render
 
     expect(rendered).to have_link 'Delete'
