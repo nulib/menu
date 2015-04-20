2176c418ce33cd292a3a0ec0609b416ca28d5727
bmquinn <brendan-quinn@northwestern.edu>
2015-04-20 10:02:27 -0500

    Break out #add_location_set_display method to deal with special cases

--- app/helpers/transform_xml.rb
+++ app/helpers/transform_xml.rb
@@ -6,6 +6,7 @@ module TransformXML
       next unless node.element?
       next if node.name == 'dateSet'
       next if node.name == 'subjectSet'
+      next if node.name == 'locationSet'
       display = Nokogiri::XML::Node.new 'vra:display', doc
       all_text_nodes = node.xpath( ".//text()" ).to_a
       all_text_nodes.delete_if { |el| el.blank? }
@@ -20,6 +21,9 @@ module TransformXML
       end
       node.children.first.add_previous_sibling( display )
     end
+
+    add_location_set_display( doc )
+
     doc.to_xml
   end
 
@@ -27,5 +31,28 @@ module TransformXML
     doc = Nokogiri::XML.parse( xml )
     doc.xpath("//vra:refid[@source='Accession']").text
   end
+
+  private
+
+  def self.add_location_set_display( nokogiri_doc )
+    display = Nokogiri::XML::Node.new 'vra:display', nokogiri_doc
+    locations = nokogiri_doc.xpath("//vra:locationSet/*").children
+    location_set_display = []
+
+    # loop through locationSet child elements and prepend certain cases with source attribute
+    locations.each do |child|
+      case
+      when child['source'] == 'Voyager'
+        location_set_display << "Voyager:#{child.text}"
+      when child['source'] == 'Accession'
+        location_set_display << "Accession:#{child.text}"
+      else
+        location_set_display<< child.text
+      end
+    end
+    display.content = location_set_display.delete_if { |el| el.blank? }.join( " ; " )
+    nokogiri_doc.xpath("//vra:locationSet").children.first.add_previous_sibling( display )
+  end
+
 end
 
