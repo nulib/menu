429933dca73c261409e009a16614709485914f27
Jennifer Lindner <jenlindner@gmail.com>
2015-04-27 11:52:50 -0500

    WIP - notes

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -24,12 +24,36 @@ class ImagesController < ApplicationController
   end
 
   # GET /images/1/edit
-  def edit_this
-    #hm. we'll call the dil_api with a pid, a get. then we need to pull its xml into the editor, from json, unless it gives us xml, look into using a respond_to? format with xml. then we need to publish it, which requires a different method then the dil post. a dil put. whioch we don't have yet in images, we have a dil create or update fedora method that needs to be broken into create and update fedora.
+  def edit
+    #hm. we'll call the dil_api with a pid, a get. then we need to pull its xml into the editor, from json, unless it gives us xml, look into using a respond_to? format with xml. 
     @response = dil_api_get_vra( params[:pid] )
+
+    #get image's id, get image, assign the response to its image_xml
 
     render xml: @response
   end
+
+
+  # PATCH/PUT /images/1
+  # PATCH/PUT /images/1.json
+  def update
+    #then we need to publish it, which requires a different method then the dil post. a dil put. which we don't have yet in images, we have a dil create or update fedora method that needs to be broken into create and update fedora.
+    #@response = dil_api_get_vra( params[:pid] )
+    #get image's id, get image, assign the response to its image_xml
+    #render xml: @response
+
+
+    respond_to do |format|
+      if @image.update_attributes(image_params)
+        format.html { redirect_to @image, notice: 'Image was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @image.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 
   # POST /images
   # POST /images.json
@@ -46,19 +70,6 @@ class ImagesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /images/1
-  # PATCH/PUT /images/1.json
-  def update
-    respond_to do |format|
-      if @image.update_attributes(image_params)
-        format.html { redirect_to @image, notice: 'Image was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @image.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # POST /images/1.xml
   def save_xml
