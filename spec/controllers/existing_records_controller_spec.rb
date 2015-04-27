2fce1bffd3e59bede997657107abe5a10897430b
Jennifer Lindner <jenlindner@gmail.com>
2015-04-27 12:39:36 -0500

    controller test added -- fails

---
+++ spec/controllers/existing_records_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+require 'pp'
+
+
+RSpec.describe ExistingRecordsController, :type => :controller do
+  describe "Edit an existing record" do
+
+  # stub_request(:post, "https://127.0.0.1:3333/multiresimages/menu_publish").
+  # to_return(:status => 200, :body => "<response><returnCode>Error</returnCode><description>Failed record</description></response>", :headers => {})
+
+    it "gets the record's vra from Images app" do
+    # GET /ExistingRecords/1/edit
+
+    end
+  end
+end
