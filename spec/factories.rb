18e7e1ba35a3fc335a79e618755e05b5a0573171
bmquinn <brendan-quinn@northwestern.edu>
2015-06-26 17:02:46 -0500

    Get an existing_record image from the Images app, with view helper to be nice

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,6 +5,45 @@ FactoryGirl.define do
     job
   end
 
+  factory :existing_record do
+    pid  "inu:dil-c5275483-699b-46de-b7ac-d4e54112cb60"
+    xml  "<vra:vra xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:fn='http://www.w3.org/2005/xpath-functions' xmlns:marc='http://www.loc.gov/MARC21/slim' xmlns:mods='http://www.loc.gov/mods/v3' xmlns:vra='http://www.vraweb.org/vracore4.htm' xsi:schemaLocation='http://www.vraweb.org/vracore4.htm http://www.vraweb.org/projects/vracore4/vra-4.0-restricted.xsd' prefix='/vra:vra/vra:work:'>
+      <vra:image id='inu-dil-2559730_w' refid='inu:dil-c5275483-699b-46de-b7ac-d4e54112cb60'>
+        <!--Agents-->
+        <vra:agentSet>
+            <vra:display>United States. War Production Board ; U.S. G.P.O.</vra:display>
+            <vra:agent>
+                <vra:name type='corporate' vocab='lcnaf'>Bon Bon</vra:name>
+                <vra:attribution/>
+            </vra:agent>
+            <vra:agent>
+                <vra:name type='corporate' vocab='lcnaf'>U.S. G.P.O.</vra:name>
+            </vra:agent>
+        </vra:agentSet>
+        <vra:culturalContextSet>
+            <vra:display/>
+            <vra:culturalContext/>
+        </vra:culturalContextSet>
+        <!--Dates-->
+        <vra:dateSet>
+            <vra:display>1942</vra:display>
+            <vra:date type='creation'>
+                <vra:earliestDate>1942</vra:earliestDate>
+            </vra:date>
+        </vra:dateSet>
+        <!-- Titles -->
+        <vra:titleSet>
+            <vra:display>'Every man, woman and child is a partner'</vra:display>
+            <vra:title pref='true'>'Every man, woman and child is a partner'</vra:title>
+        </vra:titleSet>
+        <vra:worktypeSet>
+            <vra:display/>
+            <vra:worktype/>
+        </vra:worktypeSet>
+      </vra:image>
+      </vra:vra>"
+  end
+
   factory :job do
     sequence(:job_id) { |n| "#{n}" }
     
@@ -19,4 +58,5 @@ FactoryGirl.define do
 
     end
   end
+
 end
