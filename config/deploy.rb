7a2c2c2d9bf7eb1daaafd71f6d320e92f31ed33b
Jennifer Lindner <jenlindner@gmail.com>
2015-07-15 14:00:59 -0500

    allow key forwarding in deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,7 @@ set :deploy_to, '/var/www/menu'
 
 # Default value for :scm is :git
 set :scm, :git
+set :ssh_options, { :forward_agent => true }
 
 # Default value for :format is :pretty
 set :format, :pretty
@@ -69,7 +70,7 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-       execute :touch, release_path.join('tmp/restart.txt')
+     execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
