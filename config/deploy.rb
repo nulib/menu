60fed2d58a95d5d20f673f347fc1e9eac38ca4f6
Jennifer Lindner <jenlindner@gmail.com>
2015-07-15 12:44:46 -0500

    passenger:restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -69,11 +69,12 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      execute :touch, release_path.join('tmp/restart.txt')
+      #execute :touch, release_path.join('tmp/restart.txt')
+       passenger:restart
     end
   end
 
-  after :publishing, :restart
+  #after :publishing, :restart
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
