3fddfc1cce2e565346686b4544f16459e6f44ead
Jennifer Lindner <jenlindner@gmail.com>
2015-07-17 13:49:00 -0500

    added comma

--- config/deploy.rb
+++ config/deploy.rb
@@ -70,7 +70,7 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
      execute :touch, release_path.join('tmp/restart.txt')
-     execute :rake 'menu:recreate_image_tags'
+     execute :rake, 'menu:recreate_image_tags'
      #make thumbnails generate new image paths
     end
   end
