c7cd057c4f5437616e5eff1129d16b128432fb49
Jennifer Lindner <jenlindner@gmail.com>
2015-07-18 19:21:44 -0500

    invoke rake task

--- config/deploy.rb
+++ config/deploy.rb
@@ -70,24 +70,11 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
      execute :touch, release_path.join('tmp/restart.txt')
-     #make thumbnails generate new image paths
     end
-     invoke "deploy:recreate_thumbs"
-     on roles(:app) { info "Something" }
   end
 
-  task :recreate_thumbs do
-    on roles(:app) do
-      within release_path do
-        with rails_env: fetch(:rails_env) do
-          execute :rake, "menu:recreate_image_tags"
-        end
-      end
-    end
-  end
 
   after :publishing, :restart
-  #after :restart, :recreate_thumbs
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
@@ -95,6 +82,18 @@ namespace :deploy do
       # within release_path do
       #   execute :rake, 'cache:clear'
       # end
+    end
+  end
+
+  desc 'Regenerate thumbnails'
+  task :regenerate_thumbs do
+   # fail 'no task provided' unless ENV['task']
+    on roles(:app) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "menu:recreate_image_tags"
+        end
+      end
     end
   end
 
