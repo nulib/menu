615b7466898a3402e334295937f88887949a6c9d
Jennifer Lindner <jenlindner@gmail.com>
2015-10-22 11:46:12 -0500

    a new way

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Menu::Application.routes.draw do
   mount BrowseEverything::Engine => '/browse'
 
-  get 'new_records/get_record_id/' => 'new_records#get_record_id'
-
   devise_for :users
   resources :new_records do
     member do
