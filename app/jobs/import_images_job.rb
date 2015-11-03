0393c292eef5fc8c55ecbde68963e29c4f13db46
Jennifer Lindner <jenlindner@gmail.com>
2015-11-03 16:45:46 -0600

    error handling more

--- app/jobs/import_images_job.rb
+++ app/jobs/import_images_job.rb
@@ -20,7 +20,7 @@ class ImportImagesJob < ActiveJob::Base
             file = GetNewRecords.prefix_file_name_with_job_id( file, job_id )
             f = File.open( file )
             i = job.new_records.create( filename: File.basename(file), proxy: f, location: location)
-            raise StandardError.new("Failed to create record for job: #{job_id}")
+            raise StandardError.new("Failed to create record for job: #{job_id}") if i.nil?
             f.close
           end
 
@@ -29,21 +29,22 @@ class ImportImagesJob < ActiveJob::Base
       end
   end
 
-  # def success(job)
-  #   logger.info("job  #{job_id} was successful rejoice")
-  # end
+  def success(job)
+    logger.info("job  #{job_id} was successful rejoice")
+  end
 
-  # def failure(job)
-  #   email_us_emergency(job)
-  # end
+  def failure(job)
+    email_us_emergency(job)
+  end
 
-  # def error(job, exception)
-  #   puts "Effffff job #{job_id} failed because #{exception}"
-  #   logger.error("Effffff job #{job_id} failed because #{exception}")
-  # end
+  def error(job, exception)
+    puts "job #{job_id} failed because #{exception}"
+    logger.error(" job #{job_id} failed because #{exception}")
+    #get Airbrake.notify(exception)
+  end
 
-  # def email_us_emergency(job, exception)
-  #   #OHMIGOD
-  # end
+  def alert_us_emergency(job, exception)
+    #OHMIGOD
+  end
 
 end
