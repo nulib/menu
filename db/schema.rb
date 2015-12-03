45042ea7f064cca2c69006c7fa0a82d005e499a8
Jennifer Lindner <jenlindner@gmail.com>
2015-12-03 11:15:42 -0600

    token col for db

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,23 @@
 
 ActiveRecord::Schema.define(version: 20151119164144) do
 
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
+  add_index "delayed_jobs", ["queue"], name: "delayed_jobs_queue"
+
   create_table "existing_records", force: :cascade do |t|
     t.string "pid"
     t.string "accession_number"
