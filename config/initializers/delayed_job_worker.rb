450f29dda4eb060586af88e188e96efd487903c9
Jennifer Lindner <jenlindner@gmail.com>
2015-12-16 11:45:35 -0600

    set max dj attempts to 3

--- config/initializers/delayed_job_worker.rb
+++ config/initializers/delayed_job_worker.rb
@@ -1,3 +1,4 @@
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
 Delayed::Worker.default_queue_name = 'image_importing'
+Delayed::Worker.max_attempts = 3
