2314efec28700f2b9b049001db6e70262fb376c2
Jennifer Lindner <jenlindner@gmail.com>
2016-05-31 10:43:07 -0500

    modifying test stub, fixture xml

--- spec/jobs/post_to_images_spec.rb
+++ spec/jobs/post_to_images_spec.rb
@@ -7,18 +7,21 @@ RSpec.describe PostToImagesJob, type: :job do
     before :each do
       Rake::Task["jobs:clear"].invoke
       ActionMailer::Base.deliveries.clear
-      @xml = File.read("#{Rails.root}/spec/fixtures/vra_no_display.xml")
-      @path  = "#{Rails.root}/spec/fixtures/internet.tiff"
+      @xml = File.read("#{Rails.root}/spec/fixtures/vra_display_with_content.xml")
+
+      @path = "#{Rails.root}/spec/fixtures/internet.tiff"
       @accession_nbr = "12367"
       @controller = ExistingRecordsController.new
       @new_record = NewRecord.new(:job_id => 1, :filename => "#{@path}")
       @new_record.save!
-      stub_request(:post, "https://127.0.0.1:3333/multiresimages").with(:body => {"accession_nbr"=>"12367", "from_menu"=>"true", "path"=>"#{@path}", "xml" => @xml}, :headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'Content-Length'=>'4168', 'Content-Type'=>'application/x-www-form-urlencoded', 'User-Agent'=>'Ruby'}).to_return(:status => 200, :body => "<response><returnCode>Publish successful</returnCode><pid>inu:dil-321hdsjfhjs778</pid></response>", :headers => {})
+      body = {"accession_nbr" => @accession_nbr, "from_menu"=>"true", "path" => @path,"xml" => @xml}
+      stub_request(:post, "http://127.0.0.1:3333/multiresimages").
+      with(:body => body,
+       :headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'Content-Length'=>'5924', 'Content-Type'=>'application/x-www-form-urlencoded', 'User-Agent'=>'Ruby'})
+       .to_return(:status => 200, :body => "<response><returnCode>Publish successful</returnCode><pid>inu:dil-321hdsjfhjs778</pid></response>", :headers => {})
     end
 
     it "will enqueue a job" do
-      expect(Delayed::Job.count).to eq(0)
-
       Delayed::Job.enqueue PostToImagesJob.new(@xml, @path, @accession_nbr, @new_record.id, user.email)
 
       expect(Delayed::Job.count).to eq(1)
@@ -26,8 +29,8 @@ RSpec.describe PostToImagesJob, type: :job do
 
 
     it "will perform an enqueued job" do
+      expect(Delayed::Job.count).to eq(0)
       Delayed::Job.enqueue PostToImagesJob.new(@xml, @path, @accession_nbr, @new_record.id, user.email)
-
       Delayed::Worker.new.work_off
 
       expect(Delayed::Job.count).to eq(0)
